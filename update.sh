be6c649460200347099788338d4f51b3013b4aa6
li <lilm2@ziroom.com>
2024-01-18 10:17:58 +0800

    增加更新的脚本

--- update.sh
+++ update.sh
@@ -7,13 +7,11 @@ set -e
 sudo vuepress build docs
 
 # 进入生成的文件夹s
-rm -rf /Users/li/study-code/web
 cp -r /Users/li/study-code/blog/site /Users/li/study-code
 
 cd /Users/li/study-code/site
-git init
-git add -A
+git add .
 git commit -m 'deploy'
-git push -f git@github.com:erlieStar/blog.git master:gh-pages
+git push orgin master master:gh-pages
 
 cd -
